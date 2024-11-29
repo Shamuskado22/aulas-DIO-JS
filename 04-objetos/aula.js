dce7d0cb779df05867f988cb26711574670ba095
Shamuskado22 <samucaoliveira1995@gmail.com>
2024-11-29 00:17:11 -0300

    Aula de classes finalizada

--- 04-objetos/aula.js
+++ 04-objetos/aula.js
@@ -3,44 +3,37 @@ class Pessoa {
   idade;
   altura;
   anoDeNascimento;
-  
+
   constructor(nome, idade, altura, anoDeNascimento) {
-    const dataAtual = new Date()
-    
-    this.nome = 'teste'
-    this.idade = 20
-    this.altura = 1.50
-    this.anoDeNascimento = dataAtual.getFullYear() - this.idade
-  }
+    const dataAtual = new Date();
+
+    this.nome = nome
+    this.idade = idade
+    this.altura = altura
+    anoDeNascimento = dataAtual.getFullYear() - this.idade
+    this.anoDeNascimento = anoDeNascimento
+  };
 
   descrever() {
-    console.log(`Meu nome é ${this.nome}, minha idade é ${this.idade}, nasci no ano de ${this.anoDeNascimento} e minha altura é ${this.altura}`)
+    console.log(`Meu nome é ${this.nome}, minha idade é ${this.idade}, nasci no ano de ${this.anoDeNascimento} e minha altura é ${this.altura}`);
+  };
+};
+
+function compararPessoas(p1, p2) {
+  if (p1.idade > p2.idade) {
+    console.log(`${p1.nome} é mais velho(a) que ${p2.nome}`);
+  } else if (p2.idade > p1.idade) {
+    console.log(`${p2.nome} é mais velho(a) que ${p1.nome}`);
+  } else {
+    console.log(`${p1.nome} e ${p2.nome} tem a mesma idade`)
   }
 }
 
 const samuel = new Pessoa('Samuel', 29, 1.82)
 
+const almeida = new Pessoa('Almeida', 30, 1.85)
 
-console.log(samuel)
+compararPessoas(samuel, almeida)
 
-// const pessoa = {
-//   nome: 'Samuel A. de Oliveira',
-//   idade: 29,
-//   descrever: function () {
-//     console.log(`Meu nome é ${this.nome}, minha idade é ${this.idade} e minha altura é ${this.altura}`)
-//   }
-// }
-// pessoa.altura = 1.82
-
-
-// pessoa['idade'] = 30
-// pessoa.idade = 30
-
-// samuel.descrever()
-
-// delete pessoa.nome
-
-// console.log(pessoa.nome)
-// console.log(pessoa.idade)
-// console.log(pessoa.altura)
-// console.log(pessoa)
+samuel.descrever()
+almeida.descrever()
