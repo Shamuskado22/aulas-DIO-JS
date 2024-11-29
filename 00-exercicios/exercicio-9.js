1787aee353a48fb53e000020f7167b32fadcaba3
Shamuskado22 <samucaoliveira1995@gmail.com>
2024-11-29 12:32:58 -0300

    Aula e exercício sobre Arrays e For Loop finalizadas

---
+++ 00-exercicios/exercicio-9.js
@@ -0,0 +1,8 @@
+const numeros = [5, 10, 40, 26, 95, 84, 145, 208, 1026, 312, 1995, 25, 9, 843, 0];
+
+for (let i = 0; i < numeros.length; i++) {
+  const numero = numeros[i];
+  if (numero % 2 === 0) {
+    console.log(numero);
+  };
+};
